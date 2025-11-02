a7131bbe182fdd13235db628bb844978106b2c96
Mahesh9359 <maheshjadhav9359@gmail.com>
2025-11-02 19:33:35 +0530

    debugging

--- index.php
+++ index.php
@@ -1,8 +1,6 @@
 <?php
 session_start();
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
+error_reporting(0);
 include('includes/config.php');
 
 // Initialize cart if not set
